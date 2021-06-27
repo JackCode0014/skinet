c89b56f61a72f79a717b0b3fee31ed6046491a22
cocovsdeveloper <cocovarietystore@gmail.com>
2021-06-27 23:29:47 +0800

    API Paging,Filtering,Sorting and Searching

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,12 @@ namespace API
 
             services.AddApplicationServices(); 
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                })
+            );
               
         }
 
@@ -42,6 +48,10 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseSwaggerDocumentation();
 
